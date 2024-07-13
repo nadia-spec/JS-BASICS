b79059b66ff3c89b13dfaff87fb30f25d4ef4ad0
Nadia Ibrahim <nadiaibrahim400@gmail.com>
2024-07-13 10:30:19 -0400

    js basics

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+//Objects
+let person ={
+    name:'Mosh',
+    age: 30
+};
+
+console.log(person)
+
+//Dot Notation
+person.name = 'John';
+console.log(person.name);
+
+//Bracket Notation
+person['name']='Mary';
+console.log(person.name);
